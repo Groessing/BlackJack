453a93fa289fcd2e4453cf441d3bd117ff4de6b6
Groessing <larissagroessing@gmail.com>
2025-10-07 18:15:30 +0200

    Add files via upload

---
+++ BlackJack.UI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJack.Lib;
+namespace BlackJack.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Your budget: ");
+            //int budget = Convert.ToInt32(Console.ReadLine());
+
+            Game g = new Game();
+            g.Start("Name", 1000);
+           
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1...... HIT");
+                Console.WriteLine("2...... STAND");
+                Console.WriteLine("3...... SPLIT");
+                Console.WriteLine("4...... INSURE");
+                Console.WriteLine("5...... SURRENDER");
+                Console.Write("Your choice => ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                g.MainGame(choice);
+            }
+            
+
+           // Console.ReadKey();
+        }
+    }
+}
